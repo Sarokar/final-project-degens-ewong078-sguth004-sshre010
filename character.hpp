26eeb02e738215dcf0580537d56508fa99f1ddf2
sarokar <sarokar.ca@gmail.com>
2021-05-31 18:15:16 -0700

    Added implementation to conenct with edmonds combat class

--- character.hpp
+++ character.hpp
@@ -2,22 +2,59 @@
 #define __CHARACTER_HPP__
 
 #include "person.hpp"
+#include "combat.hpp"
 #include <iostream>
 #include <string>
+#include "fire.hpp"
+#include "grass.hpp"
+#include "water.hpp"
+#include "weapons.hpp"
+#include "armor.hpp"
 
 class Character : public Person {
     private:
         double speed;
         double hp;
         double attack;
+        std::string elementType;
+
+        Fire fireType;
+        Water waterType;
+        Grass grassType;
+
+
 
     public:
-        Character(std::string name, double defaultSpd, double defaultHP, double defaultAtt) {
+        Character(std::string name, double defaultSpd, double defaultHP, double defaultAtt, std::string type) {
             setName(name);
             speed = defaultSpd;
             hp = defaultHP;
             attack = defaultAtt;
+        
+            if (type == "Fire") {
+                elementType= "Fire";
+                Fire temp(name,hp,attack,0, 0,elementType);
+                fireType = temp;
+               
+
+            }
+
+            if (type == "Water") {
+                elementType= "Water";
+               Water temp(name,hp,attack,0, 0,elementType);
+               waterType = temp;
+               
+            }
+
+            if (type == "Grass") {
+                elementType= "Grass";
+               Grass temp(name,hp,attack,0, 0,elementType);
+               grassType = temp;
+               
+            }
         }
+
+
 
         double setSpd(double toSet) {
             speed = toSet;
@@ -31,27 +68,126 @@ class Character : public Person {
             attack = toSet;
         }
 
-           //my interpretation but i dont think its that good tbh LOL
-
-       virtual std::string evaluateText(std::string toEval) {
-
-            if (toEval == "strength") {
-                return std::to_string(speed);
+        void newArmor(double armorIn) {
+            if (elementType == "Fire") {
+                fireType.setArmor(armorIn);
             }
-            else if (toEval == "hp") {
-                return std::to_string(hp);
+            if (elementType == "Water") {
+                waterType.setArmor(armorIn);
             }
-             else if (toEval == "attack") {
-                return std::to_string(attack);
-            }
-            else {
-                return "invalid";
+            if (elementType == "Grass") {
+                grassType.setArmor(armorIn);
             }
         }
 
-        virtual std::string display(std::string info) {
-          return evaluateText(info);
+        void newWeapon(double weaponIn) {
+            if (elementType == "Fire") {
+                fireType.setWeaponStrength(weaponIn);
+            }
+            if (elementType == "Water") {
+                waterType.setWeaponStrength(weaponIn);
+            }
+            if (elementType == "Grass") {
+                grassType.setWeaponStrength(weaponIn);
+            }
         }
+
+    
+
+        virtual void display(int info) {
+           evaluateText(info);
+        }
+
+        virtual void display(Character other, int characterType) {
+           evaluateText(other, characterType);
+        }
+
+        virtual void evaluateText(int toEval) {
+
+            toEval = 2;
+        }
+
+
+
+        virtual void evaluateText(Character other, int characterType) { 
+            if (characterType == 0) { // attacking enemy {
+                if (elementType == "Fire") {
+                    if (other.elementType == "Fire") { //attacking enemy
+                        fireType.calcAttack(other.fireType);
+                    }
+                    else if (other.elementType == "Water") { //attacking enemy
+                        fireType.calcAttack(other.waterType);
+                    }
+                    else { //attacking enemy
+                        fireType.calcAttack(other.grassType);
+                    }
+                }
+
+                if (elementType == "Water") {
+                    if (other.elementType == "Fire") { //attacking enemy
+                        waterType.calcAttack(other.fireType);
+                    }
+                    else if (other.elementType == "Water") { //attacking enemy
+                        waterType.calcAttack(other.waterType);
+                    }
+                    else { //attacking enemy
+                        waterType.calcAttack(other.grassType);
+                    }
+                }
+
+                if (elementType == "Grass") {
+                    if (other.elementType == "Fire") { //attacking enemy
+                        grassType.calcAttack(other.fireType);
+                    }
+                    else if (other.elementType == "Water") { //attacking enemy
+                        grassType.calcAttack(other.waterType);
+                    }
+                    else { //attacking enemy
+                        grassType.calcAttack(other.grassType);
+                    }
+                }
+            }
+
+            if (characterType == 1) { // enemy attacking  {
+                if (elementType == "Fire") {
+                    if (other.elementType == "Fire") { //enemy attacking
+                        fireType.calcEnemyAttack(other.fireType);
+                    }
+                    else if (other.elementType == "Water") { //enemy attacking
+                        fireType.calcEnemyAttack(other.waterType);
+                    }
+                    else { //enemy attacking
+                        fireType.calcEnemyAttack(other.grassType);
+                    }
+                }
+
+                if (elementType == "Water") {
+                    if (other.elementType == "Fire") { //enemy attacking
+                        waterType.calcEnemyAttack(other.fireType);
+                    }
+                    else if (other.elementType == "Water") { //enemy attacking
+                        waterType.calcEnemyAttack(other.waterType);
+                    }
+                    else { //enemy attacking
+                        waterType.calcEnemyAttack(other.grassType);
+                    }
+                }
+
+                if (elementType == "Grass") {
+                    if (other.elementType == "Fire") { //enemy attacking
+                        grassType.calcEnemyAttack(other.fireType);
+                    }
+                    else if (other.elementType == "Water") { //enemy attacking
+                        grassType.calcEnemyAttack(other.waterType);
+                    }
+                    else { //enemy attacking
+                        grassType.calcEnemyAttack(other.grassType);
+                    }
+                }
+            }
+        }
+
+        
 
 };
 
--- person.hpp
+++ person.hpp
@@ -3,12 +3,16 @@
 
 #include <iostream>
 #include <string>
+#include "character.hpp"
 
 class Person {
     protected:
         std::string name;
     
     public:
+        Person() {
+            name = "";
+        }
         virtual std::string getName() {
             return name;
         }
@@ -18,9 +22,12 @@ class Person {
 
            //my interpretation but i dont think its that good tbh LOL
 
-        virtual std::string evaluateText(std::string);
-        virtual std::string display(std::string) = 0; //i think we should change display 
-        
+        // virtual std::string evaluateText(std::string);
+        // virtual std::string display(std::string) = 0; //i think we should change display 
+        virtual void evaluateText(int) = 0;
+        virtual void display(int) = 0;
+        // virtual void evaluateText(Character, int);
+        // virtual void display(Character, int);
 
 
 };
